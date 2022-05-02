5b5fc4bbc26adba8f262471fa263785fe0875c12
Simon Tallmyr <simon@tallmyr.se>
2022-05-02 22:27:33 +0000

    First build

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,21 @@
-from app import __version__
+from typer.testing import CliRunner
+
+from app.app import app
+
+runner = CliRunner()
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_default_length():
+    result = runner.invoke(app)
+    assert result.exit_code == 0
+    assert len(result.stdout) == 17
+
+
+def test_short_password_fail():
+    result = runner.invoke(app, ["--length", "5"], input="n")
+    assert result.exit_code == 1
+
+
+def test_short_password_pass():
+    result = runner.invoke(app, ["--length", "5"], input="y")
+    assert result.exit_code == 0
